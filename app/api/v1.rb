abb08e6b542688753df2c73993b286163fa7edf4
Yura Taras <yura.taras@gmail.com>
2014-06-26 17:16:09 +0300

    Adding swagger docs for both versions

--- app/api/v1.rb
+++ app/api/v1.rb
@@ -2,4 +2,6 @@ class V1 < Grape::API
   version 'v1'
   format :json
   mount Posts::API
+
+  add_swagger_documentation base_path: '/api', api_version: 'v1'
 end
--- app/api/v2.rb
+++ app/api/v2.rb
@@ -2,4 +2,6 @@ class V2 < Grape::API
   version 'v2'
   format :json
   mount NewPosts::API
+
+  add_swagger_documentation base_path: '/api', api_version: 'v2'
 end
